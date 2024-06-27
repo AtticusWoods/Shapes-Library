f1cd6d1069e1bb0904ad29ec6649c5649f13b678
root <root@FinchComp>
2024-06-27 16:01:32 -0600

    initla rectangle and square written

--- src/main/kotlin/Rectangle.kt
+++ src/main/kotlin/Rectangle.kt
@@ -1,2 +1,21 @@
-class Rectangle {
+open class Rectangle (private var point1: Point, private var point2: Point){
+
+    open fun getArea(): Double {
+        val y = point1.getY() - point2.getY()
+        val x = point1.getX() - point2.getX()
+        return x * y
+    }
+
+    open fun getPoint1(): Point {
+        return point1
+    }
+
+    open fun getPoint2(): Point {
+        return point2
+    }
+
+    fun move(deltaX: Double, deltaY: Double) {
+        point1.move(deltaX, deltaY)
+        point2.move(deltaX, deltaY)
+    }
 }
--- src/main/kotlin/Square.kt
+++ src/main/kotlin/Square.kt
@@ -1,3 +1,4 @@
-open class Square(private var point1: Point, private var point2: Point) {
+open class Square(private var point1: Point, private var point2: Point) : Rectangle(point1, point2) {
+
 
 }
