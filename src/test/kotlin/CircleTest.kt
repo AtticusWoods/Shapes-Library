4b78cac9b162a7940df8644813e0b617a1915ab7
root <root@FinchComp>
2024-06-27 15:01:08 -0600

    Circle and Ellipse Unit Tests

--- src/test/kotlin/CircleTest.kt
+++ src/test/kotlin/CircleTest.kt
@@ -1,2 +1,34 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import kotlin.math.PI
+
 class CircleTest {
+
+    @Test
+    fun testMove() {
+        val center = Point(0.0, 0.0)
+        val radius1 = 5.0
+        val circle = Circle(center, radius1)
+        circle.move(2.0, -1.0)
+        assertEquals(2.0, circle.getCenter().getX())
+        assertEquals(-1.0, circle.getCenter().getY())
+    }
+
+    @Test
+    fun testGetPoints() {
+        val center = Point(0.0, 0.0)
+        val radius1 = 5.0
+        val square = Circle(center, radius1)
+        circle.move(2.0, -1.0)
+        assertEquals(center, circle.getCenter())
+    }
+
+    @Test
+    fun testArea() {
+        val center = Point(0.0, 0.0)
+        val radius1 = 5.0
+        val square = Circle(center, radius1)
+        assertEquals(PI * radius1 * radius1, square.getArea())
+
+    }
 }
--- src/test/kotlin/EllipseTest.kt
+++ src/test/kotlin/EllipseTest.kt
@@ -1,2 +1,37 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import kotlin.math.PI
+
 class EllipseTest {
+    @Test
+    fun testMove() {
+        val center = Point(0.0, 0.0)
+        val radius1 = 5.0
+        val radius2 = 4.0
+        val ellipse = Ellipse(center, radius1, radius2)
+        ellipse.move(2.0, -1.0)
+        assertEquals(2.0, ellipse.getCenter().getX())
+        assertEquals(-1.0, ellipse.getCenter().getY())
+    }
+
+    @Test
+    fun testGetPoints() {
+        val center = Point(0.0, 0.0)
+        val radius1 = 5.0
+        val radius2 = 4.0
+        val ellipse = Ellipse(center, radius1, radius2)
+        ellipse.move(2.0, -1.0)
+        assertEquals(center, ellipse.getCenter())
+    }
+
+    @Test
+    fun testArea() {
+        val center = Point(0.0, 0.0)
+        val radius1 = 5.0
+        val radius2 = 4.0
+        val square = Ellipse(center, radius1, radius2)
+        assertEquals(PI * radius1 * radius2, square.getArea())
+
+    }
+}
 }
