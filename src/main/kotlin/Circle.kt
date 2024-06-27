7e7d1dd45752717398fd621b268f504bdbba224a
root <root@FinchComp>
2024-06-27 15:11:38 -0600

    Circle class made

--- src/main/kotlin/Circle.kt
+++ src/main/kotlin/Circle.kt
@@ -1,2 +1,23 @@
-class Circle {
+import kotlin.math.PI
+
+open class Circle(private var center: Point, private var radius: Double) {
+    // Getter for center
+    fun getCenter(): Point {
+        return center
+    }
+
+    // Getter for radius
+    fun getRadius(): Double {
+        return radius
+    }
+
+    // Get area
+    fun getArea(): Double {
+        return PI * radius * radius
+    }
+
+    // Move the point by deltaX and deltaY
+    fun move(deltaX: Double, deltaY: Double) {
+        center.move(deltaX, deltaY)
+    }
 }
